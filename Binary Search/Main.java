33d3f5e4e877738a6be13ff471f1dde2bdbaa99f
Tidyrice-School <cheneycai2004@hotmail.com>
2022-02-11 14:49:30 -0700

    binary search init

---
+++ Binary Search/Main.java
@@ -0,0 +1,23 @@
+//BINARY SEARCH FUNCTION
+
+public class Main {
+
+    public static int[] sortedArray = {10, 30, 40, 45, 70, 80, 85, 90, 100};
+
+    public static void main(String[] args) {
+
+        System.out.println(BinarySearch(sortedArray, 90));
+
+    }
+
+    //binary search algorithm
+    private static int BinarySearch (int[] sortedArray, int term) {
+
+        int indexOf;
+        int [] scopeOfSearch = sortedArray;
+        
+        if ()
+
+        return indexOf;
+    }
+}
