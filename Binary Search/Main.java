206e98e93300d87e69a4d7528457d9b35bc70eb3
Tidyrice-School <cheneycai2004@hotmail.com>
2022-02-14 14:48:28 -0700

    Attempt #1

--- Binary Search/Main.java
+++ Binary Search/Main.java
@@ -6,18 +6,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(BinarySearch(sortedArray, 90));
+        System.out.println(BinarySearchInt(sortedArray, 90));
 
     }
 
-    //binary search algorithm
-    private static int BinarySearch (int[] sortedArray, int term) {
+    //binary search algorithm for integers
+    private static int BinarySearchInt (int[] sortedArray, int term) {
 
-        int indexOf;
-        int [] scopeOfSearch = sortedArray;
+        int lowerBound = 0;
+        int upperBound = sortedArray.length;
         
-        if ()
+        //the amount of iterations to GUARANTEE finding the term in a SORTED ARRAY is logbase2(length of array)
+        for (int i = 0; i < Math.ceil(Math.log(2) / Math.log(sortedArray.length)); i++) {
 
-        return indexOf;
+            //middleterm of the array in the bounds
+            int middleTermIndex = (int) Math.floor((upperBound - lowerBound) / 2);
+            int middleTerm = sortedArray[middleTermIndex];
+
+            //is the array's middle term the one we're trying to find?
+            if (middleTerm == term)
+                return middleTermIndex;
+
+            //the array's middle term is LARGER than the term in question (term is in bottom half of array)
+            if (middleTerm > term) {
+                upperBound = middleTermIndex - 1;
+                continue;
+            } 
+
+            //the array's middle term is SMALLER than the term in question (term is in top half of array)
+            if (middleTerm < term) {
+                lowerBound = middleTermIndex + 1;
+                continue;
+            }
+        }
+
+        //specified term is not present in array OR array is unsorted
+        return -1;
+
     }
+
+    //binary search algorithm for strings
+    //private static int BinarySearchString (String[] sortedArray, String term) {}
 }
