e477abec9e02a463669d394e078a864fd160cc4d
Tidyrice-School <cheneycai2004@hotmail.com>
2022-02-15 14:08:34 -0700

    binary search for integers complete

--- Binary Search/Main.java
+++ Binary Search/Main.java
@@ -14,13 +14,13 @@ public class Main {
     private static int BinarySearchInt (int[] sortedArray, int term) {
 
         int lowerBound = 0;
-        int upperBound = sortedArray.length;
+        int upperBound = sortedArray.length - 1;
         
         //the amount of iterations to GUARANTEE finding the term in a SORTED ARRAY is logbase2(length of array)
-        for (int i = 0; i < Math.ceil(Math.log(2) / Math.log(sortedArray.length)); i++) {
+        for (int i = 0; i < Math.ceil(Math.log(sortedArray.length) / Math.log(2)); i++) {
 
             //middleterm of the array in the bounds
-            int middleTermIndex = (int) Math.floor((upperBound - lowerBound) / 2);
+            int middleTermIndex = (int) Math.floor((upperBound + lowerBound) / 2);
             int middleTerm = sortedArray[middleTermIndex];
 
             //is the array's middle term the one we're trying to find?
