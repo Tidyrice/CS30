e4a8748e9cf22d38e0dbdfdbf2acc3126bb85fb0
Tidyrice-School <cheneycai2004@hotmail.com>
2022-02-22 14:46:50 -0700

    linear search algorithm

--- Spell Check/SpellCheck.cs
+++ Spell Check/SpellCheck.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Text.RegularExpressions;
-using BinarySearch;
+using Searches;
 
 class Program
 {
@@ -11,8 +11,12 @@ class Program
         String aliceText = System.IO.File.ReadAllText(@"data-files/AliceInWonderLand.txt");
         String[] aliceWords = Regex.Split(aliceText, @"\s+");
 
-        // Print first 50 values of each list to verify contents
-        Console.WriteLine(BinarySearch.StringToDouble("LOL"));
+        //PART A
+        Console.WriteLine("Please input a word to be checked");
+        string inputWord = Console.ReadLine();
+
+        //user picks linear or binary search
+        
     }
 
     public static void printStringArray(String[] array, int start, int stop)
@@ -23,6 +27,81 @@ class Program
             Console.WriteLine(array[i]);
         }
     }
+}
 
 
+
+
+
+namespace Searches {
+
+    public class Searches {
+
+        //linear search algorithm for strings
+        public static int LinearSearchString (string [] sortedArray, string term) {
+
+            //loop through all the terms of the array
+            for (int i = 0; i < sortedArray.length; i++) {
+                if (sortedArray[i] == term)
+                    return i;
+            }
+
+            //specified term is not present in array OR array is unsorted
+            return -1;
+
+        }
+
+        //binary search algorithm for strings
+        public static int BinarySearchString (string[] sortedArray, string term) {
+
+            //convert the string to a decimal
+            double decimalValue = StringToDouble(term);
+
+            int lowerBound = 0;
+            int upperBound = sortedArray.Length - 1;
+            
+            //the amount of iterations to GUARANTEE finding the term in a SORTED ARRAY is logbase2(length of array)
+            for (int i = 0; i < Math.Ceiling(Math.Log(sortedArray.Length) / Math.Log(2)); i++) {
+
+                //middleterm of the array in the bounds
+                int middleTermIndex = (int) Math.Floor((upperBound + lowerBound) / (decimal) 2);
+                double middleTerm = StringToDouble(sortedArray[middleTermIndex]);
+
+                //is the array's middle term the one we're trying to find?
+                if (middleTerm == decimalValue)
+                    return middleTermIndex;
+
+                //the array's middle term is LARGER than the term in question (term is in bottom half of array)
+                if (middleTerm > Math.Floor(decimalValue)) {
+                    upperBound = middleTermIndex - 1;
+                    continue;
+                } 
+
+                //the array's middle term is SMALLER than the term in question (term is in top half of array)
+                if (middleTerm < Math.Floor(decimalValue)) {
+                    lowerBound = middleTermIndex + 1;
+                    continue;
+                }
+            }
+
+            //specified term is not present in array OR array is unsorted
+            return -1;
+
+        }
+
+        //function to convert string to decimal
+        public static double StringToDouble(string text) {
+
+            //split string into an array of characters
+            char[] charArray = text.ToCharArray();
+
+            //converts each chararcter to a decimal
+            double decimalValue = 0;
+            for (int i = 0; i < charArray.Length; i++) {
+                decimalValue += charArray[i] * Math.Pow(10, -i);
+            }
+
+            return decimalValue;
+        }
+    }
 }
