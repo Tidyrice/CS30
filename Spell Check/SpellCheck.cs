9e43f99b61efeb6577920b802cb6d19abe612268
Tidyrice-School <cheneycai2004@hotmail.com>
2022-02-23 14:49:06 -0700

    2/23/2022 school

--- Spell Check/SpellCheck.cs
+++ Spell Check/SpellCheck.cs
@@ -4,29 +4,97 @@ using Searches;
 
 class Program
 {
+    //global variables
+    String[] dictionary;
+    String aliceText;
+    String[] aliceWords;
+
     public static void Main(string[] args)
     {
+
         // Load data files into arrays
-        String[] dictionary = System.IO.File.ReadAllLines(@"data-files/dictionary.txt");
-        String aliceText = System.IO.File.ReadAllText(@"data-files/AliceInWonderLand.txt");
-        String[] aliceWords = Regex.Split(aliceText, @"\s+");
+        dictionary = System.IO.File.ReadAllLines(@"data-files/dictionary.txt");
+        aliceText = System.IO.File.ReadAllText(@"data-files/AliceInWonderLand.txt");
+        aliceWords = Regex.Split(aliceText, @"\s+");
 
-        //PART A
-        Console.WriteLine("Please input a word to be checked");
-        string inputWord = Console.ReadLine();
-
-        //user picks linear or binary search
+        WhichOption();
         
     }
 
-    public static void printStringArray(String[] array, int start, int stop)
-    {
-        // Print out array elements at index values from start to stop 
-        for (int i = start; i < stop; i++)
-        {
-            Console.WriteLine(array[i]);
+    private static void WhichOption() {
+
+        int option = -1;
+
+        while (option != 5) {
+            //take user input
+            Console.WriteLine(" ");
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("  [1] Spell check single word (linear search)");
+            Console.WriteLine("  [2] Spell check single word (binary search)");
+            Console.WriteLine("  [3] Spell check Alice in Wonderland (linear search)");
+            Console.WriteLine("  [4] Spell check Alice in Wonderland (binary search)");
+            Console.WriteLine("  [5] Exit");
+            option = Console.ReadLine();
+
+            //check one word (linear)
+            if (option == 1) {
+                SingleWordCheck("linear");
+            }
+            //check one word (binary)
+            else if (option == 2) 
+            {
+                SingleWordCheck("binary");
+            }
+            //check alice in wonderland (linear)
+            else if (option == 3) 
+            {
+                AliceInWonderlandCheck("linear");
+            }
+            //check alice in wonderland (binary)
+            else if (option == 4) 
+            {
+                AliceInWonderlandCheck("binary");
+            }
         }
     }
+
+    //user inputs one word
+    private static void SingleWordCheck(string typeOfSearch) {
+        
+        //take user input
+        Console.WriteLine(" ");
+        Console.WriteLine("Word to be checked:");
+        string word = Console.ReadLine();
+
+        //get time
+        long beginTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        int index = 0;
+
+        if (typeOfSearch == "linear") {
+            index = LinearSearchString(dictionary, word);
+        } else {
+            index = BinarySearchString(dictionary, word);
+        }
+
+        //get time
+        long endTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        //output
+        Console.WriteLine(word + " is in the dictionary at index " + index);
+        Console.WriteLine("Time elapsed: " + endTime - beginTime + " milliseconds");
+    }
+
+    //check the whole alice in wonderland script
+    private static void AliceInWonderlandCheck(string typeOfSearch) {
+
+        if (typeOfSearch == "linear") {
+
+        } else {
+
+        }
+        
+    } 
 }
 
 
@@ -60,8 +128,8 @@ namespace Searches {
             int lowerBound = 0;
             int upperBound = sortedArray.Length - 1;
             
-            //the amount of iterations to GUARANTEE finding the term in a SORTED ARRAY is logbase2(length of array)
-            for (int i = 0; i < Math.Ceiling(Math.Log(sortedArray.Length) / Math.Log(2)); i++) {
+            //loop until there are no more terms to search
+            while (lowerBound <= upperBound) {
 
                 //middleterm of the array in the bounds
                 int middleTermIndex = (int) Math.Floor((upperBound + lowerBound) / (decimal) 2);
@@ -75,7 +143,7 @@ namespace Searches {
                 if (middleTerm > Math.Floor(decimalValue)) {
                     upperBound = middleTermIndex - 1;
                     continue;
-                } 
+                } else 
 
                 //the array's middle term is SMALLER than the term in question (term is in top half of array)
                 if (middleTerm < Math.Floor(decimalValue)) {
