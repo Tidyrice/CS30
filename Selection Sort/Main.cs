344bda2cf1c1a01b25d288328c0e9541551139ae
Tidyrice-School <cheneycai2004@hotmail.com>
2022-03-07 14:48:37 -0700

    selection sort. debugging

--- Selection Sort/Main.cs
+++ Selection Sort/Main.cs
@@ -10,22 +10,49 @@ public class SelectionSort {
 
         //output
         Console.WriteLine("[{0}]", string.Join(", ", SelectionSortIntArray(nums)));
-        Console.WriteLine("[{0}]", string.Join(", ", SelectionSortStringArray(words)));
+        //Console.WriteLine("[{0}]", string.Join(", ", SelectionSortStringArray(words)));
 
     }
 
     //selection sort int[]
     private static int[] SelectionSortIntArray(int[] intArray) {
+Console.WriteLine(string.Join(", ", intArray));
+        int timesIterated = 0;
 
-        //put into list
-        ArrayList unsortedList = nums.ToList();
-        ArrayList sortedList = new ArrayList();
+        while (timesIterated < intArray.Length) {
+
+            //min value trackers
+            int lowestValue = intArray[timesIterated];
+            int lowestValueIndex = timesIterated;
+
+            //first unsorted term
+            int firstUnsorted = intArray[timesIterated];
+            int firstUnsortedIndex = timesIterated;
 
 
+            //loop through each term of array
+            for (int i = timesIterated; i < intArray.Length; i++) {
+
+                //is this term smaller than the minimum recorded?
+                if (intArray[i] < lowestValue) {
+                    lowestValue = intArray[i];
+                    lowestValueIndex = i;
+                }
+
+                //swap min value with first unsorted term
+                intArray[firstUnsortedIndex] = lowestValue;
+                intArray[lowestValueIndex] = firstUnsorted;
+
+            }
+Console.WriteLine(string.Join(", ", intArray));
+            timesIterated++;
+
+        }
+
+        return intArray;
 
     }
 
     //selection sort string[]
 
-    }
 }
