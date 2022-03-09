cb49ccdd7af3ac5c2bdfb991f23f2dbfa78a285d
Tidyrice-School <cheneycai2004@hotmail.com>
2022-03-09 14:39:35 -0700

    insertion sort init

---
+++ Insertion Sort/Main.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class InsertionSort {
+
+    //test arrays
+    private static int[] nums= {10, 70, 30, 100, 40, 45, 90, 80, 85};
+    private static string[] words = {"dog","at", "good", "eye", "cat", "ball", "fish"};
+
+    public static void Main(string[] args) {
+
+        //output
+        Console.WriteLine("[{0}]", string.Join(", ", InsertionSortIntArray(nums)));
+        Console.WriteLine("[{0}]", string.Join(", ", InsertionSortStringArray(words)));
+
+    }
+
+    //insertion sort int[]
+    private static int[] InsertionSortIntArray(int[] array) {
+
+
+
+        return array;
+
+    }
+
+    //insertion sort string[]
+    private static string[] InsertionSortStringArray(string[] array) {
+
+
+
+        return array;
+
+    }
+}
