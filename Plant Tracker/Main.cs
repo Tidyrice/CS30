5635117e840b800ff665b49d716255a2ff7a0597
Tidyrice-School <cheneycai2004@hotmail.com>
2022-04-25 14:44:55 -0600

    2022-04-25

--- Plant Tracker/Main.cs
+++ Plant Tracker/Main.cs
@@ -9,20 +9,23 @@ public class PlantTracker {
     public static void Main(string[] args) {
 
         //save file exists
-        if (System.IO.File.Exists(@"data/users.json")) 
+        if (System.IO.File.Exists(@"users.json")) 
         {
+
+            users = LoadData();
+            HomeScreen();
 
         }
 
         //save file does not exist
         else 
         {
+
             users = new User[0];
             HomeScreen();
+
         }
 
-        // Load data into array
-        //users = LoadData();
         
     }
 
@@ -32,7 +35,7 @@ public class PlantTracker {
         int choice = -1;
 
         //if the choice number does not match any of the options, repeat
-        while (choice < 0 || choice > users.Length) {
+        while (choice < 0 || choice > users.Length + 1) {
 
             Console.WriteLine(" ");
             Console.WriteLine("Please select a user or add a user");
@@ -47,27 +50,30 @@ public class PlantTracker {
             //add user option
             Console.WriteLine("[" + users.Length + "] Add user");
 
+            //exit application option
+            Console.WriteLine("[" + (users.Length + 1) + "] Exit");
+
             //take input
             choice = Int32.Parse(Console.ReadLine());
 
             //user selects the "add user" option
             if (choice == users.Length) {
 
-                Console.WriteLine(" ");
-                Console.WriteLine("Please provide a username for the new user");
+                AddUser();
 
-                //get the username
-                string username = Console.ReadLine();
+            } 
+            
+            //user selects the "exit option
+            else if (choice == users.Length + 1) {
 
-                //resize users array
-                Array.Resize(ref users, users.Length + 1);
+                return;
 
-                //put new user into last position
-                users[users.Length - 1] = new User {
+            }
 
-                    username = username
+            //an existing user is selected
+            else {
 
-                };
+                PlantScreen(choice);
 
             }
 
@@ -76,18 +82,61 @@ public class PlantTracker {
 
     }
 
+    //load existing user
+    private static void PlantScreen(int userIndex) {
+
+        //get the current user
+        User currentUser = users[userIndex];
+
+        
+
+    }
+
+    //add user
+    private static void AddUser() {
+
+        string username = "";
+
+        //wait until they input an actual username
+        while (username == "") {
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Please provide a username for the new user");
+
+            //get the username
+            username = Console.ReadLine();
+
+            //*****BONUS: MAKE IT ILLEGAL TO HAVE DUPLICATE USERS*****
+
+        }
+
+        //resize users array
+        Array.Resize(ref users, users.Length + 1);
+
+        //put new user into last position
+        users[users.Length - 1] = new User {
+
+            username = username
+
+        };
+
+        SaveData();
+        HomeScreen();
+
+    }
+
     //save the users and their plants to a JSON
-    public static void SaveData(User[] users) {
+    private static void SaveData() {
 
         string json = new JavaScriptSerializer().Serialize(users);
-        System.IO.File.WriteAllText(@"data/users.json", json);
+        System.IO.File.WriteAllText(@"users.json", json);
 
     }
 
     //load the saved data
-    public static User[] LoadData() {
+    private static User[] LoadData() {
 
-        string json = System.IO.File.ReadAllText(@"data/users.json");
+        string json = System.IO.File.ReadAllText(@"users.json");
         User[] users = new JavaScriptSerializer().Deserialize<User[]>(json);
 
         return users;
@@ -98,7 +147,6 @@ public class PlantTracker {
 
 public class Plant {
 
-    public string user;
     public string name;
     public DateTime datePlanted;
     public DateTime lastWatered;
